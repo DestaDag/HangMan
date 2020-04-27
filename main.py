2f90046f8199d37f07fa044279cea906084295f8
DestaDag <dagimay1@gmail.com>
2020-04-27 19:05:59 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
 import random
-life = 8
-nouns = open("nouns.txt", "r")
 
-if nouns.mode == 'r':
-    contents = nouns.readlines()
+life = 8
+file  = open("nouns.txt", "r")
+if file.mode == "r":
+    contents = file.readlines()
 
 guess = contents[random.randint(0, len(contents))]
+guess1 = []
+
+def scan(word, letter):
+    s = 0
+    for let in range(len(word) - 1):
+        if letter == word[let]:
+            s += 1
+
+    if s == 0:
+        return True
+
+    return False        
+
+def ins(word, letter, word1):
+    for let in range(len(word)-1):
+        if letter == word[let]:
+            word1[let] = letter
+    print(word1)
+
+def final(word1):
+    s = 0
+    for x in word1:
+        if x == '_':
+           s +=1
+    
+    if s > 0:
+        return False
+    else: return True
+
 print(guess)
 for x in range(len(guess)-1):
+    guess1.append('_')
     print("_")
 
-y = input("enter letter ")
-for let in range(len(guess)-1):
-    if y == guess[let]:
-        print(y)
-    else:
-        print("_")
+while life > 0:
+    print(life)
+    inputs = ""
+    y = input("enter letter ")
+    inputs += y
+    print(inputs)
+    
+    if scan(guess, y):
+        print("Wrong letter")
+        print(guess1)
+        life -= 1
+    else: ins(guess, y, guess1)
+
+    if final(guess1):
+        print("You won")
+        break
